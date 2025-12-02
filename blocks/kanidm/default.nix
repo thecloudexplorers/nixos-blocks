cddb75a4ad9c93f1610883a0d5f00cfd42d9a391
Rutger Putter <rutger@prutser.net>
2025-12-02 11:03:19 +0100

    chore(kanidm): add options for role and package

--- blocks/kanidm/default.nix
+++ blocks/kanidm/default.nix
@@ -12,40 +12,60 @@
         description = "Top level domain on which kanidm will work";
         type = lib.types.str;
         default = "example.tld";
-        };
       };
     };
+    package = lib.mkOption {
+      description = "KanIDM package version";
+      type = lib.types.package;
+      default = "pkgs.kanidm_1_8";
+    };
+    role = lib.mkOption {
+      description = "The role of this server. This affects the replication relationship and thereby available features.";
+      default = "WriteReplica";
+      type = lib.types.enum [
+        "WriteReplica"
+        "WriteReplicaNoUI"
+        "ReadOnlyReplica"
+      ];
+    };
+  };
 
   config =
-  let
-    cfg-domain = config.domain;
-  in {
-    # enable kanidm service with config
-    services.kanidm = {
+    let
+      cfg-domain = config.domain;
+      cfg-package = config.package;
+      cfg-role = config.role;
+    in
+    {
+      # enable kanidm service with config
+      services.kanidm = {
         enableServer = true;
-        # package = pkgs.kanidm_1_7;
+        package = cfg-package;
         serverSettings = {
-        domain = cfg-domain;
-        origin = "https://login.${cfg-domain}";
-        tls_key = "/var/lib/acme/login.${cfg-domain}/key.pem";
-        tls_chain = "/var/lib/acme/login.${cfg-domain}/full.pem";
-        role = "WriteReplica";
-        ldapbindaddress = "[::]:636";
-        bindaddress = "[::]:443";
+          domain = cfg-domain;
+          origin = "https://login.${cfg-domain}";
+          tls_key = "/var/lib/acme/login.${cfg-domain}/key.pem";
+          tls_chain = "/var/lib/acme/login.${cfg-domain}/full.pem";
+          role = cfg-role;
+          ldapbindaddress = "[::]:636";
+          bindaddress = "[::]:443";
         };
-    };
+      };
 
-    # open only secure firewall ports
-    networking.firewall.allowedTCPPorts = [ 636 443 ];
+      # open only secure firewall ports
+      networking.firewall.allowedTCPPorts = [
+        636
+        443
+      ];
 
-    # make sure the certs are there before kanidm starts
-    systemd.services.kanidm = {
+      # make sure the certs are there before kanidm starts
+      systemd.services.kanidm = {
         wants = [ "acme-login.${cfg-domain}.service" ];
         after = [ "acme-login.${cfg-domain}.service" ];
-    };
+      };
 
-    # make acme certificates accessible by kanidm
-    security.acme.defaults.group = "certs";
-    users.groups.certs.members = [ "kanidm" ];
-  };
+      # make acme certificates accessible by kanidm
+      security.acme.defaults.group = "certs";
+      users.groups.certs.members = [ "kanidm" ];
+    };
 }

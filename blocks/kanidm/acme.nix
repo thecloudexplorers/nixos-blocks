6c43ecd4a5051193dde85269e707080167b2e1bb
Rutger Putter <rutger@prutser.net>
2025-12-22 13:20:26 +0100

    chore(kanidm): only activate acme when server is enabled

--- blocks/kanidm/acme.nix
+++ blocks/kanidm/acme.nix
@@ -1,11 +1,12 @@
-{ config, ... }:
+{ config, lib, ... }:
 {
   config =
     let
+      cfg-server-enable = config.nixos-blocks.kanidm.options.server.enable;
       cfg-domain = config.nixos-blocks.kanidm.options.domain.top-level;
     in
     {
-      security.acme = {
+      security.acme = lib.mkIf cfg-server-enable {
         defaults.reloadServices = [ "kanidm" ];
         certs."login.${cfg-domain}" = {
           domain = "login.${cfg-domain}";
